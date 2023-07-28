351e16f42a62ba01f356f0f09fd345332b9ff785
jsandoval86 <juancsandoval86@gmail.com>
2023-07-27 21:17:42 -0500

    java functional y stream api

--- src/main/java/org/makaia/Main.java
+++ src/main/java/org/makaia/Main.java
@@ -3,8 +3,12 @@ package org.makaia;
 import org.makaia.list.CustomGenericStack;
 import org.makaia.list.CustomIntStack;
 
-import java.time.temporal.ChronoUnit;
+import java.time.LocalDateTime;
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -12,6 +16,55 @@ public class Main {
         DataBase db = new DataBase();
 
         List<Passenger> passengers = new ArrayList<>(db.getAllPassengers());
+        buildDataStructures(db, passengers);
+
+        LocalDateTime time = LocalDateTime.now();
+
+        Supplier<LocalDateTime> function2 = LocalDateTime::now;
+        Function<String, Passenger> createFunction = Main::create;
+
+        // son equivalentes
+        List<Passenger> doctorsPassengers2 = filterPassengers(passengers, Main::isCareer);
+        List<Passenger> doctorsPassengers = filterPassengers(
+            passengers,
+            passenger1 -> passenger1.isCareer(Career.DOCTOR)
+        );
+        List<Passenger> passengersOver65Years = filterPassengers(
+            passengers,
+            passenger1 -> passenger1.getAge() > 65
+        );
+        List<Passenger> passengersLess18Years = filterPassengers(
+            passengers,
+            passenger1 -> passenger1.getAge() < 18
+        );
+
+
+        // streams
+        List<Passenger> passengersOver65YearsOld = passengers
+            .stream()
+            .filter( passenger -> {
+                return passenger.getAge() > 65 && passenger.isCareer(Career.DOCTOR);
+            })
+            .collect(Collectors.toList());
+
+        List<String> doctorsNamePassengers = passengers
+            .stream()
+            .filter( passenger -> passenger.isCareer(Career.DOCTOR) )
+            .map( passenger -> passenger.getName())
+            .collect(Collectors.toList());
+
+    }
+
+    // passenger1 -> passenger1.isCareer(Career.DOCTOR)
+    public static boolean isCareer(Passenger passenger) {
+        return passenger.isCareer(Career.DOCTOR);
+    }
+
+    public static Passenger create(String name) {
+        return new Passenger(name, 20, "none", Nacionality.COLOMBIAN.name(),"CALI");
+    }
+
+    private static void buildDataStructures(DataBase db, List<Passenger> passengers) {
         // Lists, Arrays
         List<Passenger> passengersLess18Years = getPassengersLess18Years(passengers);
         List<Passenger> passengersOver65YearsOld = getPassengersOver65YearsOld(passengers);
@@ -30,7 +83,6 @@ public class Main {
         // queues
         Queue<Passenger> passengerQueue = db.getAllPassengersOrderByAge();
         board(passengerQueue);
-
     }
 
     private static void board(Queue<Passenger> passengerQueue) {
@@ -73,6 +125,26 @@ public class Main {
         return map;
     }
 
+    private static int getAverage(List<Passenger> passengers) {
+        int sumAge = 0;
+        for (Passenger passenger: passengers) {
+            sumAge = sumAge + passenger.getAge();
+        }
+        int average = sumAge / passengers.size();
+        return average;
+    }
+
+    private static List<Passenger> filterPassengers(List<Passenger> passengersToFilter, Predicate<Passenger> predicate) {
+        List<Passenger> filteredPassengers = new ArrayList<>();
+
+        for (Passenger passenger: passengersToFilter) {
+            if (predicate.test(passenger)) {
+                filteredPassengers.add(passenger);
+            }
+        }
+        return filteredPassengers;
+    }
+
     private static List<Passenger> getPassengersDoctorProfession(List<Passenger> passengers) {
         List<Passenger> passengersDoctorProfession = new ArrayList<>();
 
@@ -82,15 +154,6 @@ public class Main {
             }
         }
         return passengersDoctorProfession;
-    }
-
-    private static int getAverage(List<Passenger> passengers) {
-        int sumAge = 0;
-        for (Passenger passenger: passengers) {
-            sumAge = sumAge + passenger.getAge();
-        }
-        int average = sumAge / passengers.size();
-        return average;
     }
 
     private static List<Passenger> getPassengersOver65YearsOld(List<Passenger> passengers) {
